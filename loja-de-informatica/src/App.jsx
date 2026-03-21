c2f788cc28b1f86bc792a9ed5e754bc2efea332e
Elisa <eeimymiura@gmail.com>
2026-03-21 15:13:31 -0300

    app.jsx

--- loja-de-informatica/src/App.jsx
+++ loja-de-informatica/src/App.jsx
@@ -3,15 +3,33 @@ import dropDown from './compnents/dropDown/dropDown';
 
 function App() {
 
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setProduto({
+      ...produto,
+      [name]: value
+    });
+  }
+
   return (
     <div>
-      <h1>Dados do produto</h1>
-      
-      <h3>Seção</h3>
-      <dropDown />
 
-      <h3>Marca</h3>
-      <dropDown />
+      <h1>Dados do produto</h1>
+
+      <form action="">
+
+        <dropDown 
+        label = "Seção"
+        name = "secao"
+        options = {[Computadores, Acessórios, Impressoras, Games, Gadgets]}
+        onChange = {handleChange}
+        />
+
+        <h3>Marca</h3>
+        <dropDown />
+
+      </form>
 
     </div>
   );
